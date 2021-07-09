22c329ac8ac586dee0e3447dff62ab9da563cafa
Chris <chris.ware.dev@gmail.com>
2021-07-09 15:24:17 -0700

    Framer Motion Updates

--- src/App.js
+++ src/App.js
@@ -7,20 +7,27 @@ import Products from "./components/pages/Products/Products";
 import SignUp from "./components/pages/SignUp/SignUp";
 import Footer from "./components/pages/Footer/Footer";
 import ScrollToTop from "./components/ScrollToTop";
+import { motion } from "framer-motion"; 
+
 
 function App() {
   return (
-    <Router>
-      <ScrollToTop />
-      <Navbar />
-      <Switch>
-        <Route path='/' exact component={Home} />
-        <Route path='/services' component={Services} />
-        <Route path='/products' component={Products} />
-        <Route path='/sign-up' component={SignUp} />
-      </Switch>
-      <Footer />
-    </Router>
+    <motion.div
+      animate={{ opacity: 1, transition: { duration: 1 } }}
+      initial={{ opacity: 0 }}
+    >
+      <Router>
+        <ScrollToTop />
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/services' component={Services} />
+          <Route path='/products' component={Products} />
+          <Route path='/sign-up' component={SignUp} />
+        </Switch>
+        <Footer />
+      </Router>
+    </motion.div>
   );
 }
 
